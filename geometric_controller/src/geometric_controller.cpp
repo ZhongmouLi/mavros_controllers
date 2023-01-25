e09a80f6d265d346ba775af3f23ddb743be1c1b1
Zhongmou <robin.zhongmou.li@gmail.com>
2023-01-25 12:15:23 +0000

    add: param for setting takeoff

--- geometric_controller/src/geometric_controller.cpp
+++ geometric_controller/src/geometric_controller.cpp
@@ -571,10 +571,12 @@ Eigen::Vector3d geometricCtrl::controlPosition(const Eigen::Vector3d &target_pos
   const Eigen::Vector3d a_rd = R_ref * D_.asDiagonal() * R_ref.transpose() * target_vel;  // Rotor drag
 
   // Reference acceleration
-  const Eigen::Vector3d a_des = a_fb + a_ref - a_rd - g_;
+  //const Eigen::Vector3d a_des = a_fb + a_ref - a_rd - g_;
+   const Eigen::Vector3d a_des = a_fb - g_;
   ROS_INFO_STREAM("a_fb is " << a_fb.transpose());
-  ROS_INFO_STREAM("a_ref is " << a_ref.transpose());
-  ROS_INFO_STREAM("a_rd is " << a_rd.transpose());
+  ROS_INFO_STREAM("a_des is " << a_des.transpose());
+  // ROS_INFO_STREAM("a_ref is " << a_ref.transpose());
+  // ROS_INFO_STREAM("a_rd is " << a_rd.transpose());
   return a_des;
 }
 
@@ -599,7 +601,15 @@ void geometricCtrl::computeBodyRateCmd(Eigen::Vector4d &bodyrate_cmd, const Eige
 Eigen::Vector3d geometricCtrl::poscontroller(const Eigen::Vector3d &pos_error, const Eigen::Vector3d &vel_error) {
   Eigen::Vector3d a_fb =
       Kpos_.asDiagonal() * pos_error + Kvel_.asDiagonal() * vel_error;  // feedforward term for trajectory error
+  ROS_INFO_STREAM("pose error is " << pos_error.transpose());
 
+  Eigen::Vector3d coneffort_pose = Kpos_.asDiagonal() * pos_error;
+  ROS_INFO_STREAM("pose control effort is " << coneffort_pose.transpose());
+  ROS_INFO_STREAM("vel error is " << vel_error.transpose());
+
+  Eigen::Vector3d coneffort_vel = Kvel_.asDiagonal() * vel_error;
+
+  ROS_INFO_STREAM("vel control effort is " << coneffort_vel.transpose());
   if (a_fb.norm() > max_fb_acc_)
     a_fb = (max_fb_acc_ / a_fb.norm()) * a_fb;  // Clip acceleration if reference is too large
 
@@ -701,6 +711,11 @@ Eigen::Vector4d geometricCtrl::geometric_attcontroller(const Eigen::Vector4d &re
   const Eigen::Vector3d zb = rotmat.col(2);
   ratecmd(3) =
       std::max(0.0, std::min(0.8, norm_thrust_const_ * ref_acc.dot(zb) + norm_thrust_offset_));  // Calculate thrust
+  
+  ROS_INFO_STREAM("ref_acc is " << ref_acc.transpose());
+  ROS_INFO_STREAM("zb is " << zb.transpose());
+  ROS_INFO_STREAM("ref_acc dot zb is " << ref_acc.dot(zb));
+  ROS_INFO_STREAM("thurst is " << ratecmd(3));
 
   return ratecmd;
 }
